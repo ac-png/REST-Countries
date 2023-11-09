586e295d2f36671d0d3b9e171332ca577dd7bd8b
ac-png <alicecorry@icloud.com>
2023-11-09 22:36:00 +0000

    Adding the search functionality to the Region page

--- src/pages/Region.js
+++ src/pages/Region.js
@@ -1,8 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Row,Col } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 import axios from "axios";
-
 import { Spinner } from "react-bootstrap";
 import CountryCard from '../components/CountryCard';
 
@@ -10,44 +9,54 @@ const Region = () => {
     const { regionName } = useParams();
 
     const [region, setRegion] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
         axios
-        .get(`https://restcountries.com/v3.1/region/${regionName}`)
-        .then((response) => {
-            const sortedCountries = [...response.data].sort((a, b) => a.name.common.localeCompare(b.name.common));
-
-            setRegion(sortedCountries);
-        })
-        .catch((error) => {
-        console.log(error);
-        });
+            .get(`https://restcountries.com/v3.1/region/${regionName}`)
+            .then((response) => {
+                const sortedCountries = [...response.data].sort((a, b) => a.name.common.localeCompare(b.name.common));
+                setRegion(sortedCountries);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }, [regionName]);
 
     if (!region) {
-        return (
-            <Spinner animation="grow" />
-        );
+        return <Spinner animation="grow" />;
     }
+
+    const filteredCountries = region.filter((country) =>
+        country.name.common.toLowerCase().startsWith(searchTerm.toLowerCase())
+    );
 
     return (
         <div className="p-4">
-        <Row xs={1} sm={2} md={3} lg={4} className="g-3">
-            {region.map((country, i) => (
-                <Col key={i}>
-                    <CountryCard
-                        flag={country.flags.png}
-                        name={country.name.common}
-                        region={country.region}
-                        population={country.population}
-                        capital={country.capital}
-                    />
-                </Col>
-            ))}
-        </Row>
-    </div>
+            <Form.Group className="mb-3">
+                <Form.Control
+                    type="text"
+                    placeholder="Search for a country"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    style={{ width: '50%' }}
+                />
+            </Form.Group>
+            <Row xs={1} sm={2} md={3} lg={4} className="g-3">
+                {filteredCountries.map((country, i) => (
+                    <Col key={i}>
+                        <CountryCard
+                            flag={country.flags.png}
+                            name={country.name.common}
+                            region={country.region}
+                            population={country.population}
+                            capital={country.capital}
+                        />
+                    </Col>
+                ))}
+            </Row>
+        </div>
     );
 }
-
 
 export default Region;
